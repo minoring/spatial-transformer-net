dd6d8f2095052fddfc159ff04ceda77e286b2935
minoring <minho1350@gmail.com>
2019-11-21 21:18:22 -0800

    Implement tf2.0 functional api

--- cluttered_mnist.py
+++ cluttered_mnist.py
@@ -30,50 +30,42 @@ def run(flags_obj):
   y_test = mnist_cluttered['y_test']
 
   X_train = np.reshape(X_train, (-1, 40, 40, 1))
-
-  train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train))
-  valid_ds = tf.data.Dataset.from_tensor_slices((X_valid, y_valid))
-  test_ds = tf.data.Dataset.from_tensor_slices((X_test, y_test))
+  print(X_train.shape)
 
   BATCH_SIZE = 256
-  train_ds.shuffle(10000).batch(batch_size=BATCH_SIZE)
 
-  inputs = tf.keras.Input(shape=(40, 40, 1,))
+  inputs = tf.keras.Input(shape=(
+      40,
+      40,
+      1,
+  ))
+  locnet = tf.keras.layers.Flatten()(inputs)
+  locnet = tf.keras.layers.Dense(20, input_shape=(40, 40, 1),
+                                 activation='tanh')(locnet)
+  locnet = tf.keras.layers.Dropout(rate=0.2)(locnet)
+  locnet = tf.keras.layers.Dense(6, activation='tanh')(locnet)
 
-  localisation_net = tf.keras.Sequential([
-      tf.keras.layers.Flatten(),
-      tf.keras.layers.Dense(20, input_shape=(40, 40, 1), activation='tanh'),
-      tf.keras.layers.Dropout(rate=0.2),
-      tf.keras.layers.Dense(6, activation='tanh')
-  ])
+  h_transformed = Transformer((40, 40))([inputs, locnet])
 
-  h_transformed = transformer(inputs, localisation_net(inputs), (40, 40))
+  h_transformed = tf.reshape(h_transformed, (-1, 40, 40, 1))
+  conv_net = tf.keras.layers.Conv2D(16,
+                                    3,
+                                    strides=(2, 2),
+                                    padding='same',
+                                    activation='relu')(h_transformed)
+  conv_net = tf.keras.layers.Conv2D(16,
+                                    3,
+                                    strides=(2, 2),
+                                    padding='same',
+                                    activation='relu')(conv_net)
+  conv_net = tf.keras.layers.Flatten()(conv_net)
+  conv_net = tf.keras.layers.Dense(1024, activation='relu')(conv_net)
+  outs = tf.keras.layers.Dense(10)(conv_net)
 
-  conv_net = tf.keras.Sequential([
-      tf.keras.layers.Input(shape=(40, 40, 1)),
-      tf.keras.layers.Conv2D(16,
-                             3,
-                             strides=(2, 2),
-                             padding='same',
-                             activation='relu'),
-      tf.keras.layers.Conv2D(16,
-                             3,
-                             strides=(2, 2),
-                             padding='same',
-                             activation='relu'),
-      tf.keras.layers.Flatten(),
-      tf.keras.layers.Dense(1024, activation='relu'),
-      tf.keras.layers.Dense(10)
-  ])
-
-  out = conv_net(h_transformed)
-
-  spatial_transformer_nets = tf.keras.Model(inputs=train_ds, outputs=out)
-
-  spatial_transformer_nets(train_ds)
+  spatial_transformer_nets = tf.keras.Model(inputs=inputs, outputs=outs)
 
   # tf.keras.utils.plot_model(spatial_transformer_nets)
-  
+
   # # Train in minibatches and report accuracy, loss
   # n_epochs = 300
   # steps_per_epoch = 10000 // BATCH_SIZE
--- transformer.py
+++ transformer.py
@@ -6,7 +6,9 @@ class Transformer(tf.keras.layers.Layer):
 
   def __init__(self, out_size):
     super(Transformer, self).__init__()
+    self.out_size = out_size
 
-  def call(self, input_tensor, theta, out_size):
-    output = transformer(input_tensor, theta, out_size)
+  def call(self, tensors):
+    input_img, theta = tensors
+    output = transformer(input_img, theta, self.out_size)
     return output
